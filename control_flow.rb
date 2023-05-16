a42c97d812f887ef940a01783b94c54b6bac8b5a
Komenpurity <jepkorirpurity8@gmail.com>
2023-05-16 11:12:04 +0300

    completed assignment

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,74 @@
 def admin_login(username, password)
-  # your code here
+
+  if (username == "sudo" && password == "12345")
+    puts "Access denied"
+
+   elsif (username == "admin" && password == "12345") 
+    puts "Access granted"
+
+   elsif (username == "ADMIN" && password == "12345") 
+    puts "Access granted"
+  end
 end
+
+admin_login("admin", "12345")
+
 
 def hows_the_weather(temperature)
-  # your code here
+
+  if(temperature < 40)
+    puts "It's brisk out there!"
+
+  elsif (temperature >= 40 || temperature <= 65 )
+    puts "It's a little chilly out there!"
+
+  elsif (temperature > 85)
+    puts "It's too dang hot out there!"
+
+  else 
+    puts "It's perfect out there!"
+  end 
+
 end
+hows_the_weather(40) 
+
 
 def fizzbuzz(num)
-  # your code here
+  
+  if (num%3 == 0)
+    puts "Fizz"
+  elsif (num%5 == 0)
+    puts "Buzz"
+  elsif (num%3 == 0 && num%5 == 0 ) 
+    puts "Fizzbuzz"
+  else
+    puts "num" 
+  end
+
 end
+fizzbuzz(100)
+
 
 def calculator(operation, num1, num2)
-  # your code here
+  
+  case operation
+    when (operation == "+")
+      puts "#{num1} + #{num2}" 
+
+    when (operation == "-")
+      puts "#{num1} - #{num2}"
+
+    when (operation == "*")
+      puts "#{num1} * #{num2}"
+
+    when (operation == "/")
+      puts "#{num1} / #{num2}" 
+
+    else
+      puts "Invalid operation!"
+      return nil 
+
+    end
+  
 end
 
